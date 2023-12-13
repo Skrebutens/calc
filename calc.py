0f479d3779031c77c978be08cb114f5e031e7f44
Arturs Skrebutens <you@example.com>
2023-12-13 10:22:32 +0200

    calc

--- calc.py
+++ calc.py
@@ -1,24 +1,30 @@
 
 # konvertē datu mērvienības
 def megabit_to_megabyte(megabit):
-   result = 0
-   return result
+    result = megabit * 8
+    return result
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
-   return result
+    result = (litres * 100)/kilometers
+    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
     result = 0
+    result = celsius * 9 / 5 + 32
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
+    a=1
     result = 0
+    for i in range(tail)
+        result = result + a
+        a += 1
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
 def weight(grams):
     return str(grams) + "g"
+    
